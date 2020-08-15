4719d222d15425255cd19350a2fa08f12d9f5c0d
Ligy <791444095@qq.com>
2020-08-15 09:40:40 +0800

    上传WindowsApi

---
+++ WinApi/WinStruts.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CSharpLib.WinApi
+{
+    public class WinStruts
+    {
+        public struct INPUT
+        {
+            public uint Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+        public struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+#pragma warning restore 649
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public Rect(Rect Rectangle) : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom)
+            {
+            }
+            public Rect(int Left, int Top, int Right, int Bottom)
+            {
+                X = Left;
+                Y = Top;
+                this.Right = Right;
+                this.Bottom = Bottom;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Left { get => X; set => X = value; }
+
+            public int Top { get => Y; set => Y = value; }
+
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+
+            public int Height
+            {
+                get { return Bottom - Y; }
+                set { Bottom = value + Y; }
+            }
+            public int Width
+            {
+                get { return Right - X; }
+                set { Right = value + X; }
+            }
+            public Point Location
+            {
+                get { return new Point(Left, Top); }
+                set
+                {
+                    X = value.X;
+                    Y = value.Y;
+                }
+            }
+            public Size Size
+            {
+                get { return new Size(Width, Height); }
+                set
+                {
+                    Right = value.Width + X;
+                    Bottom = value.Height + Y;
+                }
+            }
+
+            public static implicit operator Rectangle(Rect Rectangle)
+            {
+                return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+            }
+            public static implicit operator Rect(Rectangle Rectangle)
+            {
+                return new Rect(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+            }
+            public static bool operator ==(Rect Rectangle1, Rect Rectangle2)
+            {
+                return Rectangle1.Equals(Rectangle2);
+            }
+            public static bool operator !=(Rect Rectangle1, Rect Rectangle2)
+            {
+                return !Rectangle1.Equals(Rectangle2);
+            }
+
+            public override string ToString()
+            {
+                return "{Left: " + X + "; " + "Top: " + Y + "; Right: " + Right + "; Bottom: " + Bottom + "}";
+            }
+
+            public override int GetHashCode()
+            {
+                return ToString().GetHashCode();
+            }
+
+            public bool Equals(Rect Rectangle)
+            {
+                return Rectangle.Left == X && Rectangle.Top == Y && Rectangle.Right == Right && Rectangle.Bottom == Bottom;
+            }
+
+            public override bool Equals(object Object)
+            {
+                if (Object is Rect)
+                {
+                    return Equals((Rect)Object);
+                }
+                else if (Object is Rectangle)
+                {
+                    return Equals(new Rect((Rectangle)Object));
+                }
+
+                return false;
+            }
+        }
+    }
+}
--- WinApi/WindowsApi.cs
+++ WinApi/WindowsApi.cs
@@ -6,7 +6,7 @@ namespace CSharpLib.WinApi
     /// <summary>
     /// Windows系统API
     /// </summary>
-    internal class WindowsApi
+    public class WindowsApi
     {
         /// <summary>
         /// 热键注册
@@ -17,7 +17,7 @@ namespace CSharpLib.WinApi
         /// <param name="vk">按键</param>
         /// <returns>注册是否成功</returns>
         [DllImport("user32.dll", SetLastError = true)]
-        internal static extern bool RegisterHotKey(IntPtr hWnd, uint id, uint fsModifiers, uint vk);
+        public static extern bool RegisterHotKey(IntPtr hWnd, uint id, uint fsModifiers, uint vk);
 
 
         /// <summary>
@@ -27,7 +27,7 @@ namespace CSharpLib.WinApi
         /// <param name="id">唯一ID</param>
         /// <returns>注销结果</returns>
         [DllImport("user32.dll", SetLastError = true)]
-        internal static extern bool UnregisterHotKey(IntPtr hWnd, uint id);
+        public static extern bool UnregisterHotKey(IntPtr hWnd, uint id);
 
         /// <summary>
         /// GuidID转换成全局唯一Id
@@ -35,7 +35,7 @@ namespace CSharpLib.WinApi
         /// <param name="lpStr">字符串</param>
         /// <returns></returns>
         [DllImport("kernel32.dll")]
-        internal static extern uint GlobalAddAtom(string lpStr);
+        public static extern uint GlobalAddAtom(string lpStr);
 
         /// <summary>
         /// 删除该全局唯一ID
@@ -43,7 +43,7 @@ namespace CSharpLib.WinApi
         /// <param name="uInt16">Id</param>
         /// <returns></returns>
         [DllImport("kernel32.dll")]
-        internal static extern uint GlobalDeleteAtom(uint uInt16);
+        public static extern uint GlobalDeleteAtom(uint uInt16);
 
 
         /// <summary>
@@ -55,6 +55,100 @@ namespace CSharpLib.WinApi
         /// <param name="lParam">内容句柄</param>
         /// <returns></returns>
         [DllImport("user32")]
-        private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+
+        /// <summary>
+        /// 设置父窗体
+        /// </summary>
+        /// <param name="hWndChild">子窗体句柄</param>
+        /// <param name="hWndNewParent">父窗体句柄</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        /// <summary>
+        /// 窗体展示
+        /// </summary>
+        /// <param name="hWnd">窗体句柄</param>
+        /// <param name="nCmdShow">Max:3 Min:6 Normal:1</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        /// <summary>
+        /// 查找窗体句柄
+        /// </summary>
+        /// <param name="lpClassName">类名</param>
+        /// <param name="lpWindowName">窗体标题</param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        /// <summary>
+        /// 设置窗体标题栏
+        /// </summary>
+        /// <param name="handle">窗体句柄</param>
+        /// <param name="oldStyle">旧样式</param>
+        /// <param name="newStyle">新样式</param>
+        /// <remarks>
+        /// 无边框  句柄,-16, 369164288
+        /// </remarks>
+        [DllImport("user32.dll")]
+        public static extern void SetWindowLong(IntPtr handle, int oldStyle, int newStyle);
+
+        /// <summary>
+        /// 结束任务
+        /// </summary>
+        /// <param name="hWnd">窗体句柄</param>
+        /// <param name="fShutDown">是否立即关闭</param>
+        /// <param name="fForce">焦点</param>
+        /// <remarks>
+        /// fShutDown = true will kill the window instantly; false will show the message box before closing for Saving Changes
+        /// </remarks>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool EndTask(IntPtr hWnd, bool fShutDown, bool fForce);
+
+        /// <summary>
+        /// 获取焦点窗体句柄
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        /// <summary>
+        /// 设置窗体获取焦点
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        /// <summary>
+        /// 设置窗体位置
+        /// </summary>
+        /// <param name="hWnd">窗体句柄</param>
+        /// <param name="hWndInsertAfter">BOTTOM：值为1，将窗口置于Z序的底部。如果参数hWnd标识了一个顶层窗口，则窗口失去顶级位置，并且被置在其他窗口的底部。NOTOPMOST：值为-2，将窗口置于所有非顶层窗口之上（即在所有顶层窗口之后）。如果窗口已经是非顶层窗口则该标志不起作用。TOP：值为0，将窗口置于Z序的顶部。TOPMOST：值为-1，将窗口置于所有非顶层窗口之上。即使窗口未被激活窗口也将保持顶级位置。</param>
+        /// <param name="x">X坐标</param>
+        /// <param name="y">Y坐标</param>
+        /// <param name="cX">宽度</param>
+        /// <param name="cY">高度</param>
+        /// <param name="wFlags">具体操作</param>
+        /// <remarks>
+        /// Move:0x0001 ,Size:0x002 ,Hide:0x0080, Show:0x0040 , SetTop:1|2
+        /// </remarks>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cX, int cY, int wFlags);
+
+        /// <summary>
+        /// 获取窗体Rectangle
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpRect"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out WinStruts.Rect lpRect);
     }
 }
